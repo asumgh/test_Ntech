5d02679b2773572919541b2bd60ad5d140c1bfb8
Zaev Roman <63601919+dead-mazai@users.noreply.github.com>
2020-09-24 01:00:26 +0300

    Update tesing.py

--- Task_1_Programming/tesing.py
+++ Task_1_Programming/tesing.py
@@ -1,34 +1,13 @@
-def max_el(x):
-
-	maxs = x[0]
-
-	for i in x:
-		if maxs<i:
-			maxs = i
-
-	return maxs
-
-
-def summ(x):
-
-	s = 0
-
-	for i in x:
-		s += i 
-
-	return s
-
-
 def findMaxSubArray(a):
 
-	max_sum = max_el(a)
-	list_max = [max_el(a)]
+	max_sum = max(a)
+	list_max = [max(a)]
 
-	for lists in [[a[j:j + i] for j in range(0, len(a) - i + 1) if summ(a[j:j + i])>max_sum] for i in range(1, len(a) + 1)]:
+	for lists in [[a[j:j + i] for j in range(0, len(a) - i + 1) if sum(a[j:j + i])>max_sum] for i in range(1, len(a) + 1)]:
 		for slice in lists:
-			if max_sum<summ(slice):
+			if max_sum<sum(slice):
 
-				max_sum = summ(slice)
+				max_sum = sum(slice)
 				list_max = slice
 
 	return list_max
